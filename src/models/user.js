f1d7da2af8ab1f4b9d06ddc82fd6eba8c9bac051
Roshan Shirbhate <roshanshirbhate654@gmail.com>
2025-12-21 18:03:55 +0530

    Added the validation over data that requred to fill while signing up into app

--- src/models/user.js
+++ src/models/user.js
@@ -2,14 +2,57 @@ const mongoose = require('mongoose')
 const {Schema} = mongoose;
 
 const userSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    password: String,
-    age: Number,
-    gender : String
+    firstName: {
+        type:String,
+        require: true,
+        minLength:2,
+        maxLength:50,
+    },
+    lastName: {
+        type:String,
+        require: true,
+        minLength:4,
+        maxLength:50,
+        
+          
+    },
+    email: {
+        type:String,
+        require: true,
+        unique: true,
+        lowercase:true,
+        trim:true
+    },
+    password: {
+        type:String,
+        require: true
+    },
+    age:{
+        type:String,
+        min:18
+    },
+    gender : {
+        type:String,
+        validate(value){   
+            if (!['male', 'female', 'others'].includes(value)){
+                throw new Error("Gender is not valid")
+            }
+        }
+    },
+      
+    photoUrl:{
+        type: "String",
+        default: "/Images/dummyPhoto"
+    },
+    about:{
+        type: String,
+        default : "Hi, I am a Web Developer"
+    },
+    skill:{
+        type:[String]
+    }
 
-})
+},{timestamps: true})
 
 const User = mongoose.model("User", userSchema)
 module.exports = User
